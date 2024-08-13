2a5963b69e82af8e82607a64c99a9b639990de0d
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2024-08-13 00:50:41 -0300

    feat: print tokens and lexemes

--- E2/main.c
+++ E2/main.c
@@ -1,2 +1,39 @@
-/* main.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include "token.h"
 
+/* interface to lexer */
+
+extern char *yytext;
+extern int yylex();
+
+int main(int argc, char **argv)
+{
+    token_t token;
+
+    while ((token = yylex()))
+    {
+        switch (token)
+        {
+        case ERROR:
+            printf("ERROR %s\n", yytext);
+            break;
+        case NUMBER:
+            printf("constante decimal: %s\n", yytext);
+            break;
+        case PLUS:
+            printf("operador de adição: %s\n", yytext);
+            break;
+        case MINUS:
+            printf("operador de subtração: %s\n", yytext);
+            break;
+        case TIMES:
+            printf("operador de multiplicação: %s\n", yytext);
+            break;
+        case DIV:
+            printf("operador de divisão: %s\n", yytext);
+            break;
+        }
+        printf("código do token: %d\n", token);
+    }
+}
